5b5537b09f9ba948a386ac5a608d3030ff304435
Mavin <Mavin.anbazhagan@yahoo.com>
2024-03-12 19:40:55 -0500

    Login Auth and cache is completed

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './Apps/Screens/LoginScreen/Login';
-
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
   return (
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey='pk_test_bm9ibGUtcGlnbGV0LTczLmNsZXJrLmFjY291bnRzLmRldiQ'>
     <View style={styles.container}>
-      <Login/>
+{/*Sign In Component*/}
+      <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+{/*Sign Out Component*/}
+        <SignedOut>
+        <Login/>
+        </SignedOut>
       <StatusBar style="auto" />
     </View>
+    </ClerkProvider>
   );
 }
 
--- Apps/Screens/LoginScreen/Login.jsx
+++ Apps/Screens/LoginScreen/Login.jsx
@@ -1,8 +1,30 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../../Utils/Colors'
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
+import { useOAuth } from "@clerk/clerk-expo";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+ 
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View style={{alignItems:'center'}}>
       <Image
@@ -17,7 +39,7 @@ export default function Login() {
 <Text style={{fontSize:18, color:Colors.White, textAlign:'center', marginTop: 20}}>
   Best App to find services near you which deliver you a professional service
 </Text>
-<TouchableOpacity style={styles.button} onPress={()=>console.log("Button Click")}>
+<TouchableOpacity style={styles.button} onPress={onPress}>
   <Text style={{textAlign:'center', fontSize:17, color:Colors.Primary}}>
     Let's Get Started
   </Text>
